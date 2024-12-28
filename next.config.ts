a05024f9d9e55d17bfbdacd1158f1bd5ac97f141
Pranav Rebala <94732433+PranavR-11@users.noreply.github.com>
2024-12-28 15:25:27 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portfolio', // Replace 'portfolio' with your repository name
+  assetPrefix: '/portfolio', // Replace 'portfolio' with your repository name
 };
 
 export default nextConfig;
