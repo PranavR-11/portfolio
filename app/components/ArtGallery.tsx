06fa3edccb5f6da0b95db29a67ee010a26644efc
Pranav <pranav.rebala11@gmail.com>
2025-01-04 00:16:23 -0800

    Images

--- app/components/ArtGallery.tsx
+++ app/components/ArtGallery.tsx
@@ -21,64 +21,64 @@ const ArtGallery = () => {
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
                     <Image
-            src="/art/dws.jpeg"
+            src="/portfolio/art/dws.jpeg"
             alt="Die With A Smile Art"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
                     <Image
-            src="/art/chandler.jpeg"
+            src="/portfolio/art/chandler.jpeg"
             alt="Chandler Art"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
                     <Image
-            src="/art/dv.jpeg"
+            src="/portfolio/art/dv.jpeg"
             alt="Darth Vader Art"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
                     <Image
-            src="/art/strange.jpeg"
+            src="/portfolio/art/strange.jpeg"
             alt="Doctor Strange Art"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
                     <Image
-            src="/art/mk.jpeg"
+            src="/portfolio/art/mk.jpeg"
             alt="Moon Knight Art"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
                     <Image
-            src="/art/spidergewn.jpeg"
+            src="/portfolio/art/spidergewn.jpeg"
             alt="Spider Gwen"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
           <Image
-            src="/art/messi.jpeg"
+            src="/portfolio/art/messi.jpeg"
             alt="Messi Art"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
           <Image
-            src="/art/3spiderman.jpeg"
+            src="/portfolio/art/3spiderman.jpeg"
             alt="3 Spidermen"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
 
           <Image
-            src="/art/alvarez.jpeg"
+            src="/portfolio/art/alvarez.jpeg"
             alt="Alvarez Art"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
           <Image
-            src="/art/lucifer.jpeg"
+            src="/portfolio/art/lucifer.jpeg"
             alt="Lucifer Silhouette"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
 
           <Image
-            src="/art/painting1.jpeg"
+            src="/portfolio/art/painting1.jpeg"
             alt="Painting 1"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
           <Image
-            src="/art/painting2.jpeg"
+            src="/portfolio/art/painting2.jpeg"
             alt="Painting 2"
             className="w-full max-w-xs h-auto object-contain rounded-lg shadow-lg"
           />
